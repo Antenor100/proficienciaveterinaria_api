0e76dd5c45a4efd88330766e53b9cfed150e615f
Antenor100 <antenor_manuel2010@hotmail.com>
2022-05-17 22:59:15 -0300

    Implementações e dependências basicas do Spring

---
+++ src/main/java/br/com/devantenor/proficienciaveterinaria/ProficienciaveterinariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.devantenor.proficienciaveterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProficienciaveterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProficienciaveterinariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devantenor/proficienciaveterinaria/ProficienciaveterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devantenor.proficienciaveterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProficienciaveterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
